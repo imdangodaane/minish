690e6b68efaa4e613fd919345799c210bd424257
nqui <imdangodaane@gmail.com>
2018-11-22 17:47:59 +0700

    asd

--- intek-sh.py
+++ intek-sh.py
@@ -1,30 +1,47 @@
 #!/usr/bin/env python3
-import argparse
 import cmd
 import subprocess
-
-
-def argument():
-    parser = argparse.ArgumentParser(description='built-in parser')
+import os
 
 
 class LineArgument(cmd.Cmd):
     prompt = 'intek-sh$ '
-    def do_ls(self, person):
-        subprocess.run(['ls', '-la'])
 
-    def do_exit(self, line):
+    def do_pwd(self, line):
+        print(os.environ['PWD'])
+
+    def do_cd(self, path):
+        if path:
+            pass
+
+    def do_printenv(self, variable):
+        _dict = dict(os.environ)
+        if variable:
+            if variable in _dict.keys():
+                print(_dict[variable])
+            else:
+                pass
+        else:
+            for keys, values in _dict.items():
+                print(keys + '=' + values)
+
+    def do_ls(self, line):
+        subprocess.run(['ls', '-lA'])
+
+    def do_cl(self, line):
+        subprocess.run(['clear'])
+
+    def do_exit(self, code):
+        print('exit')
         exit()
 
-
-def basic_loop():
-    while True:
-        inp = str(input('intek-sh$ '))
+    def emptyline(self):
+        pass
 
 
 def main():
-    basic_loop()
+    LineArgument().cmdloop()
 
 
 if __name__ == '__main__':
-    LineArgument().cmdloop()
+    main()
